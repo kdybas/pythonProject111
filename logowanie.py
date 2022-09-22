cb781d1dcc5bf889d4296d0cb4b2b8cf04c3eec2
kdybas <kamilbtme@gmail.com>
2022-09-22 15:07:43 +0200

    test

--- logowanie.py
+++ logowanie.py
@@ -1,14 +1,14 @@
 import re
 
-def choices():
-    choice = int(input("For Sign In type 1 and for Register type 2: "))
-    if choice == 1:
-        return signin()
-    elif choice == 2:
-        return register()
-    else:
-        print("Bad number, type 1 or 2.\n")
-        return choices()
+#def choices():
+#    choice = int(input("For Sign In type 1 and for Register type 2: "))
+#    if choice == 1:
+#        return signin()
+#    elif choice == 2:
+#        return register()
+#    else:
+#        print("Bad number, type 1 or 2.\n")
+#        return choices()
 
 
 def signin():
