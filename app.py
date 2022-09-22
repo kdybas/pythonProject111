02f84973c8cda66e236851d922cf72316c1b6400
kdybas <112871463+kdybas@users.noreply.github.com>
2022-09-22 15:30:05 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -5,16 +5,21 @@ app = Flask(__name__)
 
 @app.route('/test', methods=['POST'])
 def test():
-    abc = 1
+    f = open("User_Data.txt", 'r')
+    info = f.read()
+    info = info.split()
     res = {"is_exist": False,
          "is_correct": False}
-    if abc == 1:
+    if request.json.get("login") in info:
         res["is_exist"] = True
-
+        index = info.index(request.json.get("login")) + 1
+        usr_password = info[index]
+        if request.json.get("haslo") == usr_password:
+            res["is_correct"] = True
     yres = json.dumps(res)
-
     print(request.json)
     return yres
+
 
 
 if __name__ == '__main__':
